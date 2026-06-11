9623202d14061f9ed2fe8a1f51e4fb5bd42d4056
Sajesh Jose <hello@dotyello.com>
2026-06-11 14:59:27 -0400

    Initial Commit

---
+++ BetFit/BetFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetFitApp.swift
+//  BetFit
+//
+//  Created by Sajesh on 2026-06-11.
+//
+
+import SwiftUI
+
+@main
+struct BetFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
